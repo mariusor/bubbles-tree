6578017df3729287b864d8a95defecc70f36947f
Marius Orcsik <marius@habarnam.ro>
2021-04-25 10:05:50 +0200

    Styling the error message and the debug messages

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/marius/bubbles-tree
 
 go 1.16
 
-require github.com/charmbracelet/bubbletea v0.13.2 // indirect
+require (
+	github.com/charmbracelet/bubbletea v0.13.2 // indirect
+	github.com/charmbracelet/lipgloss v0.2.0 // indirect
+)
--- tree.go
+++ tree.go
@@ -3,6 +3,7 @@ package tree
 import (
 	"fmt"
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 	"strings"
 )
 
@@ -90,13 +91,23 @@ func (m Model) View() string {
 	return m.render()
 }
 
+var (
+	errForegroundColor = lipgloss.AdaptiveColor{ Light: "#E03F3F", Dark: "#F45B5B" }
+	errBackgroundColor = lipgloss.AdaptiveColor{ Light: "#212121", Dark: "#4A4A4A" }
+	errStyle = lipgloss.Style{}.Foreground(errForegroundColor).Background(errBackgroundColor)
+
+	debugForegroundColor = lipgloss.AdaptiveColor{ Light: "#FFA348", Dark: "#FFBE6F" }
+	debugBackgroundColor = lipgloss.AdaptiveColor{ Light: "#212121", Dark: "#4A4A4A" }
+	debugStyle = lipgloss.Style{}.Foreground(debugForegroundColor).Background(debugBackgroundColor)
+)
+
 func (m Model) render() string {
 	bot := clamp(m.cur.top+m.cur.h, m.cur.h, len(m.tree))
 	top := clamp(m.cur.top, 0, max(len(m.tree)-m.cur.h, m.cur.h))
 	if Debug {
-		DebugLines = append(DebugLines, fmt.Sprintf("w:h %d:%d", m.cur.w, m.cur.h))
-		DebugLines = append(DebugLines, fmt.Sprintf("t:b %d:%d", top, bot))
-		DebugLines = append(DebugLines, fmt.Sprintf("lines: %d", len(m.tree)))
+		DebugLines = append(DebugLines, debugStyle.Width(m.cur.w).Render(fmt.Sprintf("w:h %d:%d", m.cur.w, m.cur.h)))
+		DebugLines = append(DebugLines, debugStyle.Width(m.cur.w).Render(fmt.Sprintf("t:b %d:%d", top, bot)))
+		DebugLines = append(DebugLines, debugStyle.Width(m.cur.w).Render(fmt.Sprintf("lines: %d", len(m.tree))))
 		if len(m.tree) > bot {
 			bot -= len(DebugLines)
 		}
@@ -106,7 +117,8 @@ func (m Model) render() string {
 		lines = m.tree[top:bot]
 	}
 	if m.Err != nil {
-		lines = append([]string{m.Err.Error()}, lines...)
+		err := []string{errStyle.Width(m.cur.w).Render(m.Err.Error())}
+		lines = append(err, lines...)
 	}
 	if Debug {
 		lines = append(DebugLines, lines...)
